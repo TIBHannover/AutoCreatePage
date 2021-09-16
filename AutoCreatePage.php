c1c70a76fea4750a4d6f99ba77b0df4e06580518
Marc Hinterthaner <uni@hinterthaner.de>
2021-09-16 15:52:46 +0200

    comments updated

--- AutoCreatePage.php
+++ AutoCreatePage.php
@@ -46,28 +46,17 @@ $GLOBALS['wgExtensionCredits']['other'][] = array(
 
 $GLOBALS['wgExtensionMessagesFiles']['AutoCreatePageMagic'] =  dirname(__FILE__) . '/AutoCreatePage.i18n.magic.php';
 
-
-
 $GLOBALS['wgExtensionFunctions'][] = function() {
 
-    wfDebugLog( 'autocreatepage', "i.0" );
-
 	$GLOBALS['wgHooks']['ParserFirstCallInit'][] = function ( \Parser &$parser ) {
-
-        wfDebugLog( 'autocreatepage', "a.0" );
 
         $parser->setFunctionHook( 'createPage', function( $parser ) {
             return createPageIfNotExisting( func_get_args() );
         } );
 
         $parser->setFunctionHook( 'append2Page', function( $parser ) {
-              wfDebugLog( 'autocreatepage', "a.1" );
               return append2Page( func_get_args() );
         } );
-
-
-
-        wfDebugLog( 'autocreatepage', "a.2" );
 	};
 
 	$GLOBALS['wgHooks']['ArticleEditUpdates'][] = 'doCreatePages';
@@ -123,12 +112,11 @@ function createPageIfNotExisting( array $rawParams ) {
 }
 
 /**
- * Creates pages that have been requested by the creat page parser function. This is done only
+ * Creates pages that have been requested by the create page parser function. This is done only
  * after the safe is complete to avoid any concurrent article modifications.
  * Note that article is, in spite of its name, a WikiPage object since MW 1.21.
  */
 function doCreatePages( &$article, &$editInfo, $changed ) {
-    wfDebugLog( 'autocreatepage', "d.0" );
 
     global $egAutoCreatePageMaxRecursion;
 
@@ -145,7 +133,6 @@ function doCreatePages( &$article, &$editInfo, $changed ) {
 
 	foreach ( $createPageData as $pageTitleText => $pageContentText ) {
 		$pageTitle = Title::newFromText( $pageTitleText );
-		// wfDebugLog( 'createpage', "CREATE " . $pageTitle->getText() . " Text: " . $pageContent );
 
 		if ( !is_null( $pageTitle ) && !$pageTitle->isKnown() && $pageTitle->canExist() ){
 			$newWikiPage = new WikiPage( $pageTitle );
@@ -153,7 +140,6 @@ function doCreatePages( &$article, &$editInfo, $changed ) {
 			$newWikiPage->doEditContent( $pageContent,
 				"Page created automatically by parser function on page [[$sourceTitleText]]" ); //TODO i18n
 
-			// wfDebugLog( 'createpage', "CREATED PAGE " . $pageTitle->getText() . " Text: " . $pageContent );
 		}
 	}
 
@@ -164,16 +150,15 @@ function doCreatePages( &$article, &$editInfo, $changed ) {
 	return true;
 }
 
-/*
- *
+/**
+ * Handles the parser function for appending content to existing pages.
+ * It is possible to use &lt;nowiki&gt;
+ * in the text parameter to insert verbatim wiki text.
  */
 
 function append2Page(array $rawParams) {
-    wfDebugLog( 'autocreatepage', "p.0" );
 
     global $egAutoCreatePageMaxRecursion, $egAutoCreatePageNamespaces;
-
-    wfDebugLog( 'autocreatepage', "p.1" );
 
     if ( isset( $rawParams[0] ) && isset( $rawParams[1] ) && isset( $rawParams[2] ) ) {
         $parser = $rawParams[0];
@@ -183,25 +168,17 @@ function append2Page(array $rawParams) {
         throw new MWException( 'Hook invoked with missing parameters.' );
     }
 
-    wfDebugLog( 'autocreatepage', "p.2" );
-
     if ( empty( $targetPageTitleText ) ) {
         return 'Error: this function must be given a valid title text for the page to which contend will be appended.';
     }
-
-    wfDebugLog( 'autocreatepage', "p.3" );
 
     // Append content to pages only if the page calling the parser function is within defined namespaces
     if ( !in_array( $parser->getTitle()->getNamespace(), $egAutoCreatePageNamespaces ) ) {
         return '';
     }
 
-    wfDebugLog( 'autocreatepage', "p.4" );
-
     // Get the raw text of $targetPageContent2Append as it was before stripping <nowiki>:
     $targetPageContent2Append = $parser->mStripState->unstripNoWiki( $targetPageContent2Append );
-
-    wfDebugLog( 'autocreatepage', "p.5" );
 
     // Store data in the parser output for later use:
     $append2PageData = $parser->getOutput()->getExtensionData( 'append2Page' );
@@ -209,83 +186,50 @@ function append2Page(array $rawParams) {
         $append2PageData = array();
     }
 
-    wfDebugLog( 'autocreatepage', "p.6" );
-
     $append2PageData[$targetPageTitleText] = $targetPageContent2Append;
     $parser->getOutput()->setExtensionData( 'append2Page', $append2PageData );
-
-    wfDebugLog( 'autocreatepage', "p.7" );
 
     return "";
 }
 
+/**
+ * Appends content to pages that have been requested by the append to page parser function. This is done only
+ * after the safe is complete to avoid any concurrent article modifications.
+ * Note that article is, in spite of its name, a WikiPage object since MW 1.21.
+ */
 function doAppend2Pages(&$article, &$editInfo, $changed) {
 
-    wfDebugLog( 'autocreatepage', "da.0" );
-
     global $egAutoCreatePageMaxRecursion;
-
-    wfDebugLog( 'autocreatepage', "da.1" );
 
     $append2PageData = $editInfo->output->getExtensionData( 'append2Page' );
     if ( is_null( $append2PageData ) ) {
         return true; // no pages to which content will be appended
     }
 
-    wfDebugLog( 'autocreatepage', "da.2" );
-
     // Prevent appending content to pages by pages to which content has been appended to avoid loops:
     $egAutoCreatePageMaxRecursion--;
-
-    wfDebugLog( 'autocreatepage', "da.3" );
-
     $sourceTitle = $article->getTitle(); // title object
     $sourceTitleText = $sourceTitle->getPrefixedText(); // displayed text of title
 
-    wfDebugLog( 'autocreatepage', "da.4" );
-
     foreach ( $append2PageData as $pageTitleText => $pageContentText2Append ) {
-        wfDebugLog( 'autocreatepage', "da.4.1" );
 
         $pageTitle = Title::newFromText( $pageTitleText );
 
-        wfDebugLog( 'autocreatepage', "da.4.2" );
-
         if ( !is_null( $pageTitle ) && $pageTitle->isKnown() ){
-            wfDebugLog( 'autocreatepage', "da.4.2.1" );
 
             $targetWikiPage = new WikiPage( $pageTitle );
-            wfDebugLog( 'autocreatepage', "da.4.2.2" );
-            //wfDebugLog( 'autocreatepage', var_export($targetWikiPage, true));
-
-
-
             $currentText = $targetWikiPage->getContent()->getText();
-            wfDebugLog( 'autocreatepage', "da.4.2.3" );
-            //wfDebugLog( 'autocreatepage', "Content null? " . (is_null($currentText) ? "true" : "false") );
-            //wfDebugLog( 'autocreatepage', "Content: " . var_export($currentText, true) );
-            //wfDebugLog( 'autocreatepage', "Content2Append: " . var_export($pageContentText2Append, true) );
-
             $newContent = ContentHandler::makeContent( $currentText . $pageContentText2Append, $sourceTitle );
-
-            wfDebugLog( 'autocreatepage', "da.4.2.4" );
 
             // $targetWikiPage->doEdit( $currentText . $pageContentText2Append,
             $targetWikiPage->doEditContent( $newContent,
                 "Content appended to Page automatically by parser function on page [[$sourceTitleText]]" );
-            wfDebugLog( 'autocreatepage', "da.4.2.5" );
         }
-
-        wfDebugLog( 'autocreatepage', "da.4.3" );
     }
-
-    wfDebugLog( 'autocreatepage', "da.5" );
 
     // Reset state. Probably not needed since parsing is usually done here anyway:
     $editInfo->output->setExtensionData( 'append2Page', null );
     $egAutoCreatePageMaxRecursion++;
-
-    wfDebugLog( 'autocreatepage', "da.6" );
 
     return true;
 }
